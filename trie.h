0d783159fa26eb38447483455e96b24c311a0e86
GDS1474-utec <giancarlo.davila@utec.edu.pe>
2026-07-01 17:19:29 -0500

    Update trie.h

--- trie.h
+++ trie.h
@@ -11,7 +11,6 @@ namespace utils
   std::vector<std::string> parseCSVLine(const std::string &line);
 }
 
-
 class Movie
 {
 public:
@@ -27,21 +26,14 @@ public:
 };
 
 /*
-Cada nodo contiene:
-  1) Un char que representa la letra de ese nodo.
-  2) Un contenedor unordered_map de los hijos de el nodo actual. Se utilizo
-     unordered_map ya que la busqueda es O(1) la mayoria de los casos, lo cual
-     nos permite navegar el arbol de manera rapida y consistente.
-  3) Un vector de IDs de peliculas asociadas a la palabra formada por el camino
-     de un nodo despues de la raiz hasta el nodo actual. Si el vector es vacio
-     sabemos que el nodo actual no es una hoja.
-  4) Un vector de enteros llamado pesos que sirve para decidir cual pelicula
-     encaja mas con la cadena dada por el usuario en busquedas.
-     De momento, si la palabra aparece en el titulo tiene un peso de 10, mientras
-     que palabras en el plot valen 5 puntos.
-  Los indices de pesos corresponden con los indices del vector de IDs, es decir,
-  si el ID de una pelicula esta en el indice 5, el peso de la cadena asociada a
-  esa pelicula tambien esta en el indice 5 de pesos.
+Trie de busqueda por caracteres.
+Cada nodo guarda un caracter y sus hijos. En los nodos terminales se guarda un
+mapa pelicula -> puntaje. Se usa puntaje para ordenar las coincidencias por
+importancia.
+
+Para permitir busqueda por sub-palabra se insertan sufijos de cada palabra.
+Ejemplo: para "desembarcar" se insertan "desembarcar", "esembarcar", ...,
+"barcar", etc. Asi una busqueda por "bar" puede encontrar la pelicula.
 */
 class TrieNode
 {
@@ -50,20 +42,20 @@ public:
   TrieNode(char letra);
 
   std::unordered_map<char, TrieNode*> children_;
-  std::vector<unsigned> movieIds_;
-  std::vector<unsigned> pesos_;
+  std::unordered_map<unsigned, unsigned> scores_; // movieId -> puntaje acumulado
   char letra_;
-  
 };
 
 class Trie
 {
 private:
   TrieNode* root_;
+
 public:
   Trie();
 
   void insert(const std::string &word, unsigned movieId, unsigned peso);
+  void insert(const std::string &word, const std::unordered_map<unsigned, unsigned> &movieScores);
   std::vector<std::pair<unsigned, unsigned>> search(const std::string &word);
 };
 
@@ -79,6 +71,7 @@ private:
   std::unordered_map<unsigned, std::vector<unsigned>> yearMap_;
   std::unordered_map<std::string, std::vector<unsigned>> originMap_;
   std::unordered_map<std::string, std::vector<unsigned>> genreMap_;
+
 public:
   enum CATEGORIA_BUSQUEDA
   {
@@ -92,6 +85,9 @@ public:
 
   void loadCSV(const std::string& filename);
   std::vector<unsigned> search(std::string str, CATEGORIA_BUSQUEDA categ);
+
+  const Movie& getMovie(unsigned id) const { return movies_.at(id); }
+  unsigned movieCount() const { return movies_.size(); }
 };
 
 class User
